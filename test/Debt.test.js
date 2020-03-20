3f67a6684fac8f58aff42319ab6d020f5c13549a
MKG <mohdkamal47@gmail.com>
2020-03-20 15:14:26 +0200

    added test conditions and e2e testing

--- test/Debt.test.js
+++ test/Debt.test.js
@@ -15,25 +15,98 @@ let factory;
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
     ;[caller] = accounts
+    amount = 1000000
+    description = "test-debt"
+    Bob = accounts[0]
+    Alice = accounts[1]
   
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
       .deploy({ data: compiledFactory.bytecode })
       .send({ from: caller, gas: '1000000' })
-
-    await factory.methods.createDebt(100000, accounts[1],"test-debt").send({
+    
+    await factory.methods.createDebt(amount, accounts[1], description, "random txHash").send({
         from: caller,
         gas: '1000000'
     })
-
-    ;[debtAddress] = await factory.methods.getDeployedDebts().call();
+    
+    deployedDebts = await factory.methods.getDeployedDebts().call();
 
     debt = await new web3.eth.Contract(JSON.parse(compiledDebt.interface),
-    debtAddress);
+    deployedDebts[0]);
 });
 
-describe("Debt Collector DeFi", ()=> {
+describe("P2P Lending & Debt Management DApp", async ()=> {
     it("deploys contracts", () => {
         assert.ok(factory.options.address)
         assert.ok(debt.options.address)
     });
+    it("allows users to lend each other money", async ()=> {
+        aliceBalance = await web3.eth.getBalance(Alice)
+        await web3.eth.sendTransaction({
+            from: Bob,
+            to: Alice,
+            value: amount
+        });
+        aliceNewBalance = await web3.eth.getBalance(Alice)
+        assert(aliceNewBalance=aliceBalance+amount)
+    });
+    it("creates a debt record between two parties", async()=> {
+        tx = await web3.eth.sendTransaction({
+            from: Bob,
+            to: Alice,
+            value: amount
+        });
+        _txHash = tx.transactionHash
+        await factory.methods.createDebt(amount, Alice, description, _txHash).send({
+        from: Bob,
+        gas: '1000000'
+        });
+        debts = await factory.methods.getDeployedDebts().call();
+        debt = await new web3.eth.Contract(JSON.parse(compiledDebt.interface),
+        debts[1]);
+        debtDetails = await debt.methods.getDetails().call();
+        assert(debtDetails[5]==_txHash)
+    });
+    it("allows borrowers to settle debt", async()=> {
+        tx = await web3.eth.sendTransaction({
+            from: Bob,
+            to: Alice,
+            value: amount
+        });
+        _txHash = tx.transactionHash
+        await factory.methods.createDebt(amount, Alice, description, _txHash).send({
+        from: Bob,
+        gas: '1000000'
+        });
+        debts = await factory.methods.getDeployedDebts().call();
+        debt = await new web3.eth.Contract(JSON.parse(compiledDebt.interface),
+        debts[1]);
+        await debt.methods.settleDebt().send({
+            from: Alice,
+            gas: '1000000'
+        });
+    });
+    it("end-to-end testing", async()=> {
+        tx = await web3.eth.sendTransaction({
+            from: Bob,
+            to: Alice,
+            value: amount
+        });
+        _txHash = tx.transactionHash
+        await factory.methods.createDebt(amount, Alice, description, _txHash).send({
+        from: Bob,
+        gas: '1000000'
+        });
+        debts = await factory.methods.getDeployedDebts().call();
+        debt = await new web3.eth.Contract(JSON.parse(compiledDebt.interface),
+        debts[1]);
+        debtDetails = await debt.methods.getDetails().call();
+        await debt.methods.settleDebt().send({
+            from: Alice,
+            gas: '1000000'
+        });
+        assert(debtDetails[5]==_txHash)
+        
+    });
+
 })
